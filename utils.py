3e6acf39d2ffc38fc340b5e26b71e385b4cd1199
Matvei <m.tarasov@innopolis.university>
2026-09-07 05:54:43 +0000

    fix TTMatVec (avoid t.numel)

--- utils.py
+++ utils.py
@@ -115,161 +115,161 @@ def tt_svd(X: t.Tensor):
 
 class TTMatVec(t.autograd.Function):
 
-    @staticmethod
-    def forward(ctx, X: t.Tensor, *cores: List[t.Tensor]):
-        d = len(cores) // 2
+	@staticmethod
+	def forward(ctx, X: t.Tensor, *cores: List[t.Tensor]):
+		d = len(cores) // 2
 
-        A_d = cores[0]
-        for i in range(1, d):
-            A_d = t.tensordot(A_d, cores[i], dims=([-1], [0]))
+		A_d = cores[0]
+		for i in range(1, d):
+			A_d = t.tensordot(A_d, cores[i], dims=([-1], [0]))
 
-        B_d = cores[d]
-        for i in range(d+1, 2*d):
-            B_d = t.tensordot(B_d, cores[i], dims=([-1], [0]))
+		B_d = cores[d]
+		for i in range(d+1, 2*d):
+			B_d = t.tensordot(B_d, cores[i], dims=([-1], [0]))
 
-        a_sh = A_d.shape
-        b_sh = B_d.shape
-        T_1 = X @ A_d.reshape((a_sh[:-1].numel(), a_sh[-1]))
-        Y = T_1 @ B_d.reshape((b_sh[0], b_sh[1:].numel()))
-        ctx.save_for_backward(X, T_1, *cores)
-        return Y
+		a_sh = A_d.shape
+		b_sh = B_d.shape
+		T_1 = X @ A_d.reshape((A_d.numel()//a_sh[-1], a_sh[-1]))
+		Y = T_1 @ B_d.reshape((b_sh[0], B_d.numel()//b_sh[0]))
+		ctx.save_for_backward(X, T_1, *cores)
+		return Y
 
-    @staticmethod
-    def backward(ctx, *g_Y):
-        X, T_1, *cores = ctx.saved_tensors
-        d = len(cores) // 2
-        r_d = t.Size([cores[d-1].shape[2]])
-        in_shape, out_shape = t.Size([]), t.Size([])
-        for n in range(d):
-            in_shape += t.Size([cores[n].shape[1]])
-        for n in range(d, 2*d):
-            out_shape += t.Size([cores[n].shape[1]])
+	@staticmethod
+	def backward(ctx, *g_Y):
+		X, T_1, *cores = ctx.saved_tensors
+		d = len(cores) // 2
+		r_d = t.Size([cores[d-1].shape[2]])
+		in_shape, out_shape = t.Size([]), t.Size([])
+		for n in range(d):
+			in_shape += t.Size([cores[n].shape[1]])
+		for n in range(d, 2*d):
+			out_shape += t.Size([cores[n].shape[1]])
 
-        # =============
+		# ============= 
+		
+		A = [cores[0]]
+		d = len(cores) // 2
+		for i in range(1, d):
+			A.append(t.tensordot(A[-1], cores[i], dims=([-1], [0])))
 
-        A = [cores[0]]
-        d = len(cores) // 2
-        for i in range(1, d):
-            A.append(t.tensordot(A[-1], cores[i], dims=([-1], [0])))
+		A_inv = [cores[d-1]]
+		for i in range(d-2, -1, -1):
+			A_inv.append(t.tensordot(cores[i], A_inv[-1], dims=([-1], [0])))
 
-        A_inv = [cores[d-1]]
-        for i in range(d-2, -1, -1):
-            A_inv.append(t.tensordot(cores[i], A_inv[-1], dims=([-1], [0])))
+		B = [cores[d]]
+		for i in range(d+1, 2*d):
+			B.append(t.tensordot(B[-1], cores[i], dims=([-1], [0])))
 
-        B = [cores[d]]
-        for i in range(d+1, 2*d):
-            B.append(t.tensordot(B[-1], cores[i], dims=([-1], [0])))
+		B_inv = [cores[2*d-1]]
+		for i in range(2*d-2, d-1, -1):
+			B_inv.append(t.tensordot(cores[i], B_inv[-1], dims=([-1], [0])))
 
-        B_inv = [cores[2*d-1]]
-        for i in range(2*d-2, d-1, -1):
-            B_inv.append(t.tensordot(cores[i], B_inv[-1], dims=([-1], [0])))
+		# =============
 
-        # =============
+		# g_X
+		A_d = A[d-1]
+		B_d = B[d-1]
+		a_sh = A_d.shape
+		b_sh = B_d.shape
 
-        # g_X
-        A_d = A[d-1]
-        B_d = B[d-1]
-        a_sh = A_d.shape
-        b_sh = B_d.shape
+		U_1 = g_Y[0] @ B_d.reshape((b_sh[0], B_d.numel()//b_sh[0])).T
+		g_X = U_1 @ A_d.reshape((A_d.numel()//a_sh[-1], a_sh[-1])).T
 
-        U_1 = g_Y[0] @ B_d.reshape((b_sh[0], b_sh[1:].numel())).T
-        g_X = U_1 @ A_d.reshape((a_sh[:-1].numel(), a_sh[-1])).T
+		# =============
 
-        # =============
+		# g_G_i: i <= d
+		U_2 = (U_1.T @ X).reshape(r_d + in_shape)
+		g_G_left = []
 
-        # g_G_i: i <= d
-        U_2 = (U_1.T @ X).reshape(r_d + in_shape)
-        g_G_left = []
+		# i = 1
+		i = 1
+		expr_U_2 = [0] + [k for k in range(1, d + 1)]
+		expr_A_inv_i = [2 * d + 4] + [k for k in range(i + 1, d + 1)] + [0]
+		expr_g_G = [i, 2 * d + 4]
 
-        # i = 1
-        i = 1
-        expr_U_2 = [0] + [k for k in range(1, d + 1)]
-        expr_A_inv_i = [2 * d + 4] + [k for k in range(i + 1, d + 1)] + [0]
-        expr_g_G = [i, 2 * d + 4]
+		g_G_1 = t.einsum(
+			U_2, expr_U_2,
+			A_inv[d-i-1], expr_A_inv_i,
+			expr_g_G
+		)[None, :, :]
+		g_G_left.append(g_G_1)
 
-        g_G_1 = t.einsum(
-            U_2, expr_U_2,
-            A_inv[d-i-1], expr_A_inv_i,
-            expr_g_G
-        )[None, :, :]
-        g_G_left.append(g_G_1)
+		# 1 < i < d
+		for i in range(2, d):
+			expr_U_2 = [0] + [k for k in range(1, d + 1)]
+			expr_A_i = [2 * d + 2] + [k for k in range(1, i)] + [2 * d + 3]
+			expr_A_inv_i = [2 * d + 4] + [k for k in range(i + 1, d + 1)] + [0]
+			expr_g_G = [2 * d + 3, i, 2 * d + 4]
 
-        # 1 < i < d
-        for i in range(2, d):
-            expr_U_2 = [0] + [k for k in range(1, d + 1)]
-            expr_A_i = [2 * d + 2] + [k for k in range(1, i)] + [2 * d + 3]
-            expr_A_inv_i = [2 * d + 4] + [k for k in range(i + 1, d + 1)] + [0]
-            expr_g_G = [2 * d + 3, i, 2 * d + 4]
+			g_G_i = t.einsum(
+				U_2, expr_U_2,
+				A[i-2], expr_A_i,
+				A_inv[d-i-1], expr_A_inv_i,
+				expr_g_G
+			)
+			g_G_left.append(g_G_i)
 
-            g_G_i = t.einsum(
-                U_2, expr_U_2,
-                A[i-2], expr_A_i,
-                A_inv[d-i-1], expr_A_inv_i,
-                expr_g_G
-            )
-            g_G_left.append(g_G_i)
+		# i = d
+		i = d
+		expr_U_2 = [0] + [k for k in range(1, d + 1)]
+		expr_A_i = [2 * d + 2] + [k for k in range(1, i)] + [2 * d + 3]
+		expr_g_G = [2 * d + 3, i, 0]
 
-        # i = d
-        i = d
-        expr_U_2 = [0] + [k for k in range(1, d + 1)]
-        expr_A_i = [2 * d + 2] + [k for k in range(1, i)] + [2 * d + 3]
-        expr_g_G = [2 * d + 3, i, 0]
+		g_G_d = t.einsum(
+			U_2, expr_U_2,
+			A[i-2], expr_A_i,
+			expr_g_G
+		)
+		g_G_left.append(g_G_d)
 
-        g_G_d = t.einsum(
-            U_2, expr_U_2,
-            A[i-2], expr_A_i,
-            expr_g_G
-        )
-        g_G_left.append(g_G_d)
+		# =============
 
-        # =============
+		# g_Gi: i >= d + 1
+		T_2 = (g_Y[0].T @ T_1).reshape(out_shape + r_d)
+		g_G_right = []
 
-        # g_Gi: i >= d + 1
-        T_2 = (g_Y[0].T @ T_1).reshape(out_shape + r_d)
-        g_G_right = []
+		# i = d + 1
+		i = d + 1
+		expr_T_2 = [k for k in range(d + 1, 2 * d + 1)] + [d]
+		expr_B_inv_i = [2 * d + 3] + [k for k in range(i + 1, 2 * d + 2)]
+		expr_g_G = [d, i, 2 * d + 3]
 
-        # i = d + 1
-        i = d + 1
-        expr_T_2 = [k for k in range(d + 1, 2 * d + 1)] + [d]
-        expr_B_inv_i = [2 * d + 3] + [k for k in range(i + 1, 2 * d + 2)]
-        expr_g_G = [d, i, 2 * d + 3]
+		g_G_d1 = t.einsum(
+			T_2, expr_T_2,
+			B_inv[2*d-i-1], expr_B_inv_i,
+			expr_g_G
+		)
+		g_G_right.append(g_G_d1)
 
-        g_G_d1 = t.einsum(
-            T_2, expr_T_2,
-            B_inv[2*d-i-1], expr_B_inv_i,
-            expr_g_G
-        )
-        g_G_right.append(g_G_d1)
+		# d + 1 < i < 2d
+		for i in range(d+2, 2*d):
+			expr_T_2 = [k for k in range(d + 1, 2 * d + 1)] + [d]
+			expr_B_i = [d] + [k for k in range(d + 1, i)] + [2 * d + 2]
+			expr_B_inv_i = [2 * d + 3] + [k for k in range(i + 1, 2 * d + 2)]
+			expr_g_G = [2 * d + 2, i, 2 * d + 3]
 
-        # d + 1 < i < 2d
-        for i in range(d+2, 2*d):
-            expr_T_2 = [k for k in range(d + 1, 2 * d + 1)] + [d]
-            expr_B_i = [d] + [k for k in range(d + 1, i)] + [2 * d + 2]
-            expr_B_inv_i = [2 * d + 3] + [k for k in range(i + 1, 2 * d + 2)]
-            expr_g_G = [2 * d + 2, i, 2 * d + 3]
+			g_G_di = t.einsum(
+				T_2, expr_T_2,
+				B[i-d-2], expr_B_i,
+				B_inv[2*d-i-1], expr_B_inv_i,
+				expr_g_G
+			)
+			g_G_right.append(g_G_di)
 
-            g_G_di = t.einsum(
-                T_2, expr_T_2,
-                B[i-d-2], expr_B_i,
-                B_inv[2*d-i-1], expr_B_inv_i,
-                expr_g_G
-            )
-            g_G_right.append(g_G_di)
+		# i = 2d
+		i = 2 * d
+		expr_T_2 = [k for k in range(d + 1, 2 * d + 1)] + [d]
+		expr_B_i = [d] + [k for k in range(d + 1, i)] + [2 * d + 2]
+		expr_g_G = [2 * d + 2, i]
 
-        # i = 2d
-        i = 2 * d
-        expr_T_2 = [k for k in range(d + 1, 2 * d + 1)] + [d]
-        expr_B_i = [d] + [k for k in range(d + 1, i)] + [2 * d + 2]
-        expr_g_G = [2 * d + 2, i]
+		g_G_2d = t.einsum(
+			T_2, expr_T_2,
+			B[i-d-2], expr_B_i,
+			expr_g_G
+		)[:, :, None]
+		g_G_right.append(g_G_2d)
 
-        g_G_2d = t.einsum(
-            T_2, expr_T_2,
-            B[i-d-2], expr_B_i,
-            expr_g_G
-        )[:, :, None]
-        g_G_right.append(g_G_2d)
+		# =============
 
-        # =============
-
-        # g = [g_X] + g_G_left + g_G_right
-        return g_X, *g_G_left, *g_G_right
+		# g = [g_X] + g_G_left + g_G_right
+		return g_X, *g_G_left, *g_G_right
